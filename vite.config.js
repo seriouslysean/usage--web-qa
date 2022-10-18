d30007fb754ff85e355a98aeb0fcb8c8b7819a2c
Sean Kennedy <sean@codeserious.com>
2022-10-18 10:59:40 -0400

    Set baseUrl for github pages

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,14 @@
 /** @type {import('vite').UserConfig} */
 
 import { resolve } from 'path';
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+
+const isProd = process.env.NODE_ENV === 'production';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: isProd ? '/web-qa-usage/' : '/',
   resolve:{
     alias:{
       '@' : resolve(__dirname, './src'),
