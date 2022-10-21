7fb0670fe48bc4648d6528f06c8e68ec5969269d
Sean Kennedy <sean@codeserious.com>
2022-10-21 19:47:30 -0400

    Adjust page styles, add complete todo, undo todo and delete todo functionality

--- src/views/TodoView.vue
+++ src/views/TodoView.vue
@@ -1,17 +1,42 @@
 <script setup>
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
 
 // TODO: Save and populate from local storage
 const todos = ref([]);
 const newTodo = ref('');
+const newTodoInput = ref(null);
+const newTodoError = ref(false);
+
+// Computed values
+const activeTodos = computed(() => todos.value.filter(todo => !todo.complete));
+const completedTodos = computed(() => todos.value.filter(todo => todo.complete));
+
+// Methods
+const todoExists = (text) => todos.value.find((todo) => todo.text === text) !== undefined;
+const setTodoError = () => {
+    newTodoError.value = todoExists(newTodo.value);
+};
 
 function addTodo() {
-    todos.value.push(newTodo.value);
+    if (todoExists(newTodo.value)) {
+        newTodoError.value = true;
+        return;
+    }
+    todos.value.push({
+        text: newTodo.value,
+        complete: false,
+    });
     newTodo.value = '';
+    newTodoInput.value.focus();
 }
 
-function removeTodo(index) {
-    todos.value.splice(index, 1);
+function removeTodo(todo) {
+    todos.value = todos.value.filter(({ text }) => todo.text !== text);
+}
+
+function toggleTodo(todo, complete) {
+    const foundTodo = todos.value.find(({ text }) => todo.text === text);
+    foundTodo.complete = complete;
 }
 </script>
 
@@ -24,11 +49,16 @@ function removeTodo(index) {
             class="todo-form"
         >
             <input
+                ref="newTodoInput"
                 type="text"
                 v-model="newTodo"
                 placeholder="Enter a new todo"
                 required
                 class="todo-form__input"
+                :class="{
+                    'has-error': newTodoError === true,
+                }"
+                @input="setTodoError"
             >
             <button
                 title="Add todo"
@@ -40,22 +70,65 @@ function removeTodo(index) {
         </form>
     </section>
 
-    <section>
+    <section class="todo-container">
         <ol class="todo-list">
             <li
-                v-for="(todo, index) in todos"
+                v-for="(todo, index) in activeTodos"
                 :key="todo"
                 class="todo-list__item"
             >
                 <p class="todo-list__text">
-                    {{ todo }}
+                    {{ todo.text }}
                 </p>
 
                 <button
-                    @click="removeTodo(index)"
-                    class="todo-list__button"
+                    title="Complete todo"
+                    @click="toggleTodo(todo, true)"
+                    class="todo-list__button todo-list__button--complete"
                 >
-                    X
+                    ✓
+                </button>
+
+                <button
+                    title="Delete todo"
+                    @click="removeTodo(todo)"
+                    class="todo-list__button todo-list__button--delete"
+                >
+                    Delete
+                </button>
+            </li>
+        </ol>
+    </section>
+
+    <section
+        v-if="completedTodos.length"
+        class="todo-container todo-container--completed"
+    >
+        <h2>Completed</h2>
+        <ol class="todo-list todo-list--completed">
+            <li
+                v-for="(todo, index) in completedTodos"
+                :key="todo"
+                class="todo-list__item"
+            >
+                <p class="todo-list__text">
+                    {{ todo.text }}
+                </p>
+
+                <button
+                    title="Undo todo"
+                    @click="toggleTodo(todo, false)"
+                    class="todo-list__button todo-list__button--undo"
+                >
+                    Undo
+                </button>
+
+                <button
+                    title="Delete todo"
+                    @click="removeTodo(todo)"
+                    class="todo-list__button todo-list__button--delete"
+                >
+                    Delete
                 </button>
             </li>
         </ol>
@@ -63,6 +136,10 @@ function removeTodo(index) {
 </template>
 
 <style lang="scss">
+.todo-container {
+    margin-top: 2em;
+}
+
 .todo-form {
     display: flex;
 
@@ -72,6 +149,10 @@ function removeTodo(index) {
         width: 100%;
         padding: 1em;
         flex-grow: 1;
+
+        &.has-error {
+            border: 1px solid red;
+        }
     }
 
     &__button {
@@ -86,17 +167,42 @@ function removeTodo(index) {
     &__item {
         display: flex;
         align-items: center;
+        position: relative;
     }
 
     &__text {
         flex-grow: 1;
         text-align: left;
+
+        .todo-container--completed & {
+            text-decoration: line-through;
+        }
     }
 
     &__button {
-        background: red;
-        justify-content: flex-end;
         margin-left: 1em;
+
+        &--complete {
+            background: green;
+        }
+
+        &--delete {
+            border: 0;
+            background: transparent;
+            padding-left: 0;
+            padding-right: 0;
+            text-transform: lowercase;
+            display: block;
+            color: red;
+            position: absolute;
+            left: calc(100% + 1em);
+            top: 0;
+
+            &:hover,
+            &:active {
+                text-decoration: underline;
+            }
+        }
     }
 
 }
